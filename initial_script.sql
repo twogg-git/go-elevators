a829025c7884ba1b95c10793e00f610d540e6bf7
Catherin Cruz <cruz.catherin@gmail.com>
2018-02-05 20:06:17 -0500

    updating schema name, required timestamp fields removing unused columns

--- initial_script.sql
+++ initial_script.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA `gofun` ;
+CREATE SCHEMA `go-elevators` ;
 
-CREATE TABLE `gofun`.`elevators` (
+CREATE TABLE `go-elevators`.`elevators` (
   `elevator_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `max_size` int(11) unsigned NOT NULL DEFAULT '1',
@@ -10,23 +10,23 @@ CREATE TABLE `gofun`.`elevators` (
   UNIQUE KEY `uniq_elevators_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `gofun`.`requests` (
+CREATE TABLE `go-elevators`.`requests` (
   `request_id` int(11) unsigned NOT NULL AUTO_INCREMENT,  
-  `elevator_id` int(11) unsigned NOT NULL,
   `person_name` varchar(32) NOT NULL,
   `initial_floor` int(11) unsigned NOT NULL,
   `destination_floor` int(11) unsigned NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `current_floor` int(11) unsigned NOT NULL,
+  `elevator_id` int(11) unsigned,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`request_id`),  
   UNIQUE KEY `uniq_requests_person_name` (`person_name`),
   UNIQUE KEY `uniq_requests_elevator_person_floor` (`elevator_id`, `person_name`, `initial_floor`,  `destination_floor`),
   CONSTRAINT `fk_requests_elevator_id` FOREIGN KEY (`elevator_id`) REFERENCES `elevators` (`elevator_id`) ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `gofun`.`operations` (
+CREATE TABLE `go-elevators`.`operations` (
   `operation_id` int(11) unsigned NOT NULL AUTO_INCREMENT,  
   `elevator_id` int(11) unsigned NOT NULL,
-  `load_size` int(11) unsigned NOT NULL DEFAULT '1',
   `is_going_up`  tinyint(4) NOT NULL DEFAULT '1',
   `current_floor` int(11) unsigned NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -35,7 +35,7 @@ CREATE TABLE `gofun`.`operations` (
   CONSTRAINT `fk_operations_elevator_id` FOREIGN KEY (`elevator_id`) REFERENCES `elevators` (`elevator_id`) ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `gofun`.`configurations` (
+CREATE TABLE `go-elevators`.`configurations` (
   `conf_key` varchar(32) NOT NULL,
   `conf_value` varchar(32) NOT NULL,
   `description` varchar(50) NOT NULL,
@@ -44,16 +44,16 @@ CREATE TABLE `gofun`.`configurations` (
   UNIQUE KEY `uniq_configurations_key` (`conf_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
   
-INSERT INTO `gofun`.`elevators` (`name`,  `max_size`,  `status`)
+INSERT INTO `go-elevators`.`elevators` (`name`,  `max_size`,  `status`)
 VALUES
   ('Elevator1', 5, 1),
   ('ElevatorOFF', 8, 0);
 
-INSERT INTO `gofun`.`configurations` ( `conf_key`,  `conf_value`,  `description`)
+INSERT INTO `go-elevators`.`configurations` ( `conf_key`,  `conf_value`,  `description`)
 VALUES
   ('floor_count', 5, 'Quantity of floors available'),
   ('elevator_delay', 2, 'Time between elevators updates. In seconds'),
   ('request_delay', 15, 'Time between requests calls. In seconds');
   
   
-  select * from `gofun`.`configurations`;
+  select * from `go-elevators`.`configurations`;
